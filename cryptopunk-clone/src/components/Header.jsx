d49e1b74e11e4dc2cf3599da4659adff55c402f4
Vincnet J Lettau II <vjerome2@live.com>
2021-12-14 12:14:32 -0500

    completed header

--- cryptopunk-clone/src/components/Header.jsx
+++ cryptopunk-clone/src/components/Header.jsx
@@ -2,6 +2,7 @@ import "./Header.css";
 
 import punkLogo from "../assets/header/cryptopunk-logo.png";
 import searchIcon from "../assets/header/search.png";
+import themeSwitch from "../assets/header/theme-switch.png";
 
 export const Header = () => {
   return (
@@ -11,7 +12,7 @@ export const Header = () => {
       </div>
       <div className="searchBar">
         <div className="searchIconContainer">
-          <img className="searchIcon" alt="search icon" src={searchIcon} />
+          <img className="searchIcon" alt="search-icon" src={searchIcon} />
         </div>
         <input
           className="searchInput"
@@ -22,7 +23,18 @@ export const Header = () => {
         />
       </div>
 
-      <div className="headerItems"></div>
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
+
+      <div className="headerActions">
+        <div className="themeSwitchContainer">
+          <img className="themeSwitch" src={themeSwitch} alt="themeswitch" />
+        </div>
+      </div>
+      <div className="loginButton">Get in</div>
     </div>
   );
 };
