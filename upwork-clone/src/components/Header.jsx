523e68380262a45782785c422f993efee02e64ca
Vincnet J Lettau II <vjerome2@live.com>
2021-12-17 21:12:26 -0500

    added dependencies @mui

--- upwork-clone/src/components/Header.jsx
+++ upwork-clone/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import icon from "../assets/icons/icon.svg";
+import { MenuIcon } from "@mui/icons-material";
 import logo from "../assets/images/logo.png";
 import styled from "styled-components";
 
 export const Header = () => {
   return (
     <Container>
-      <div>Menu</div>
+      <div className="menu"></div>
       <Content>
         <Logo />
         <AuthLinks>
@@ -18,24 +18,33 @@ export const Header = () => {
 };
 
 const Container = styled.div`
+  box-sizing: border-box;
+  margin: 0;
+  font-family: sans-serif;
   display: flex;
-  height: 52px;
+  height: 56px;
   width: 100vw;
   border-bottom: 2px solid rgba(0, 0, 0, 0.05);
+  > .menu {
+    width: 50px;
+  }
 `;
 const Content = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   justify-content: space-between;
-  padding-right: 25px;
+  padding: 5px 10px;
+  border-left: 2px solid rgba(0, 0, 0, 0.05);
 `;
 const AuthLinks = styled.div`
   display: flex;
+  width: 150px;
+  justify-content: space-around;
 `;
 const Logo = styled.div`
   background: url(${logo});
   height: 50px;
   width: 200px;
-  margin-left: 25px;
   background-repeat: no-repeat;
 `;
